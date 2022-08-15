dacc6b727ae3f784e141b41c1c8f4b69d85eb47e
Hannah Steil <h-steil@t-online.de>
2022-08-15 15:48:44 +0200

    Add basic styling to the list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
 import Head from 'next/head';
 
 import Layout from '../components/Layout';
+import StyledList from '../components/List/StyledList';
+import StyledListItem from '../components/ListItem/StyledListItem';
+import useStore from '../hooks/useStore';
 
 export default function HomePage() {
+	const tasks = useStore(state => state.tasks);
+
 	return (
 		<Layout>
 			<Head>
 				<title key="title">My Project</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Head>
-			<h1>Meine Aufgaben</h1>
+			<h1>Meine Aufgaben App</h1>
+			<h2>Aufgabenliste</h2>
+			<StyledList>
+				{tasks.map(task => (
+					<StyledListItem key={task.id}>
+						<span>{task.name}</span>
+						<span>{task.time}min</span>
+					</StyledListItem>
+				))}
+			</StyledList>
 		</Layout>
 	);
 }
