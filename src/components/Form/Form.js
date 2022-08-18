49593c8c9f3cd5e65e901b7d6322922a41855adb
Hannah Steil <h-steil@t-online.de>
2022-08-18 14:17:46 +0200

    Change variable name

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,9 +8,9 @@ export default function Form() {
 	function handleSubmit(event) {
 		event.preventDefault();
 		const form = event.target;
-		const name = form.elements.task.value.trim();
+		const taskName = form.elements.task.value.trim();
 		const duration = Number.parseInt(form.elements.duration.value, 10);
-		addTask(name, duration);
+		addTask(taskName, duration);
 		form.reset();
 	}
 
