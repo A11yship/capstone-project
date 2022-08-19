57715e2a1c4549ef67ed4b60557b495db27ca2dd
Hannah Steil <h-steil@t-online.de>
2022-08-19 12:22:29 +0200

    Add basic styling to the app

---
+++ src/components/StyledMain.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledMain = styled.main`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 20px;
+`;
+
+export default StyledMain;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import CurrentTask from '../components/CurrentTask/CurrentTask';
 import Form from '../components/Form/Form';
 import StyledList from '../components/List/StyledList';
 import StyledListItem from '../components/ListItem/StyledListItem';
+import StyledMain from '../components/StyledMain';
 import useStore from '../hooks/useStore';
 
 export default function HomePage() {
@@ -15,20 +16,22 @@ export default function HomePage() {
 				<title key="title">My Project</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Head>
-			<h1>Meine Aufgaben App</h1>
-			<h2>Aktuelle Aufgabe</h2>
-			<CurrentTask />
-			<h2>Aufgabenliste</h2>
-			<StyledList role="list">
-				{tasks.map(task => (
-					<StyledListItem key={task.id}>
-						<span>{task.name}</span>
-						<span>{task.time}min</span>
-					</StyledListItem>
-				))}
-			</StyledList>
-			<h2>Aufgabe hinzufügen</h2>
-			<Form />
+			<StyledMain>
+				<h1>Meine Aufgaben App</h1>
+				<h2>Aktuelle Aufgabe</h2>
+				<CurrentTask />
+				<h2>Aufgabenliste</h2>
+				<StyledList role="list">
+					{tasks.map(task => (
+						<StyledListItem key={task.id}>
+							<span>{task.name}</span>
+							<span>{task.time}min</span>
+						</StyledListItem>
+					))}
+				</StyledList>
+				<h2>Aufgabe hinzufügen</h2>
+				<Form />
+			</StyledMain>
 		</>
 	);
 }
