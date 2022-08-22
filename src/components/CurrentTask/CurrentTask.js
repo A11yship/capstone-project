06e0de6ca0b41dbb05ee1ff9e812373fe199e9d9
Hannah Steil <h-steil@t-online.de>
2022-08-22 14:14:43 +0200

    Add done button to current task

--- src/components/CurrentTask/CurrentTask.js
+++ src/components/CurrentTask/CurrentTask.js
@@ -1,14 +1,19 @@
 import useStore from '../../hooks/useStore';
+import Button from '../Button/Button';
 
 import StyledCurrentTask from './StyledCurrentTask';
 
 export default function CurrentTask() {
 	const tasks = useStore(state => state.tasks);
+	function handleClick() {
+		console.log('done');
+	}
 
 	return (
 		<StyledCurrentTask>
 			<p>{tasks[0].name}</p>
 			<p>{tasks[0].time} min</p>
+			<Button onClick={handleClick}>done</Button>
 		</StyledCurrentTask>
 	);
 }
