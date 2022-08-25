dce71921a50b5cf08c2322c15407924453519fc5
Hannah Steil <h-steil@t-online.de>
2022-08-25 13:17:23 +0200

    Add basic styling to the navbar

--- src/pages/create-task.js
+++ src/pages/create-task.js
@@ -1,11 +1,15 @@
 import Form from '../components/Form/Form';
+import Navbar from '../components/Navbar/Navbar';
 import StyledMain from '../components/StyledMain';
 
 export default function CreateTask() {
 	return (
-		<StyledMain>
-			<h1>Aufgabe hinzufügen</h1>
-			<Form />
-		</StyledMain>
+		<>
+			<StyledMain>
+				<h1>Aufgabe hinzufügen</h1>
+				<Form />
+			</StyledMain>
+			<Navbar />
+		</>
 	);
 }
