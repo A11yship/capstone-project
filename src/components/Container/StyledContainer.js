623f752adf73f1f5e7c329c848788bd7b79a6a21
Hannah Steil <h-steil@t-online.de>
2022-08-26 13:53:19 +0200

    Add container

---
+++ src/components/Container/StyledContainer.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+	display: flex;
+	width: auto;
+	height: calc(100vh - 50px);
+	margin: 5px;
+	border: 1px solid black;
+`;
+
+export default StyledContainer;
--- src/pages/create-list.js
+++ src/pages/create-list.js
@@ -1,7 +1,10 @@
+import StyledContainer from '../components/Container/StyledContainer';
+
 export default function CreateList() {
 	return (
 		<>
 			<h1>Wähle aktuelle Aufgaben </h1>
+			<StyledContainer>test</StyledContainer>
 		</>
 	);
 }
