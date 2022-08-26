920467f718201b694fb15877745ace27bc76d74f
Hannah Steil <h-steil@t-online.de>
2022-08-26 15:29:46 +0200

    Add tasks to columns

--- src/components/Column/StyledColumn.js
+++ src/components/Column/StyledColumn.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 const StyledColumn = styled.div`
+	display: flex;
+	flex-direction: column;
 	width: 100%;
 	padding: 5px;
 	border: 1px solid black;
--- src/pages/create-list.js
+++ src/pages/create-list.js
@@ -1,15 +1,24 @@
 import StyledColumn from '../components/Column/StyledColumn';
 import StyledContainer from '../components/Container/StyledContainer';
+import useStore from '../hooks/useStore';
 
 export default function CreateList() {
 	const columns = ['Alle Aufgaben', 'Ausgewählte Aufgaben'];
+	const tasks = useStore(state => state.tasks);
 
 	return (
 		<>
 			<h1>Wähle aktuelle Aufgaben </h1>
 			<StyledContainer>
 				{columns.map(column => {
-					return <StyledColumn key={column}>{column}</StyledColumn>;
+					return (
+						<StyledColumn key={column}>
+							{column}{' '}
+							{tasks.map(task => {
+								return <div key={task.id}>{task.name}</div>;
+							})}
+						</StyledColumn>
+					);
 				})}
 			</StyledContainer>
 		</>
