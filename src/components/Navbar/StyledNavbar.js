37643fb95b642dcf6e165367c0a3e36c2bdf0bf8
Hannah Steil <h-steil@t-online.de>
2022-08-30 14:45:53 +0200

    Fix styling bugs

--- src/components/Navbar/StyledNavbar.js
+++ src/components/Navbar/StyledNavbar.js
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 const StyledNavbar = styled.nav`
 	display: flex;
-	position: absolute;
+	position: fixed;
 	bottom: 0;
 	align-items: center;
 	justify-content: space-around;
 	width: 100vw;
 	height: 30px;
 	border: 1px solid black;
+	background-color: white;
 `;
 
 export default StyledNavbar;
