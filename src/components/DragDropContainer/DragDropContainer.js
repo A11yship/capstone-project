f18d1174fa8bbe06021ef795401c1175f632c543
Hannah Steil <h-steil@t-online.de>
2022-08-30 11:55:21 +0200

    Add functionality to onDragEnd

--- src/components/DragDropContainer/DragDropContainer.js
+++ src/components/DragDropContainer/DragDropContainer.js
@@ -10,9 +10,45 @@ export default function DragDropContainer() {
 	const [unselectedTasks, setUnselectedTasks] = useState([...tasks]);
 	const [selectedTasks, setSelectedTasks] = useState([]);
 
-	function handleOnDragEnd() {
-		setUnselectedTasks([...tasks]);
-		setSelectedTasks([]);
+	function handleOnDragEnd(result) {
+		const {destination, source, draggableId} = result;
+
+		const draggableTask = tasks.filter(task => task.id === draggableId);
+		console.log(...draggableTask);
+
+		if (!destination) {
+			return;
+		}
+
+		if (destination.droppableId === source.droppableId) {
+			if (destination.droppableId === columns[0]) {
+				const newTasks = unselectedTasks;
+				newTasks.splice(source.index, 1);
+				newTasks.splice(destination.index, 0, ...draggableTask);
+				setUnselectedTasks([...newTasks]);
+			} else if (destination.droppableId === columns[1]) {
+				const newTasks = selectedTasks;
+				newTasks.splice(source.index, 1);
+				newTasks.splice(destination.index, 0, ...draggableTask);
+				setSelectedTasks([...newTasks]);
+			}
+		} else {
+			if (source.droppableId === columns[0]) {
+				const sourceTasks = unselectedTasks;
+				sourceTasks.splice(source.index, 1);
+				const destinationTasks = selectedTasks;
+				destinationTasks.splice(destination.index, 0, ...draggableTask);
+				setUnselectedTasks([...sourceTasks]);
+				setSelectedTasks([...destinationTasks]);
+			} else if (source.droppableId === columns[1]) {
+				const sourceTasks = selectedTasks;
+				sourceTasks.splice(source.index, 1);
+				const destinationTasks = unselectedTasks;
+				destinationTasks.splice(destination.index, 0, ...draggableTask);
+				setUnselectedTasks([...destinationTasks]);
+				setSelectedTasks([...sourceTasks]);
+			}
+		}
 	}
 
 	return (
