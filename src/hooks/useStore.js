39fb01d55c1b39acca97703946988b6f540eab16
Hannah Steil <h-steil@t-online.de>
2022-09-01 13:08:12 +0200

    Add generating function

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -6,6 +6,10 @@ const useStore = create(set => ({
 		{id: nanoid(), name: 'Test Timer', time: 0.25},
 		{id: nanoid(), name: 'Abwaschen', time: 15},
 		{id: nanoid(), name: 'Post sortieren', time: 2},
+		{id: nanoid(), name: 'Task 1', time: 10},
+		{id: nanoid(), name: 'Task 2', time: 10},
+		{id: nanoid(), name: 'Task 3', time: 10},
+		{id: nanoid(), name: 'Task 4', time: 10},
 	],
 	addTask(name, time) {
 		set(({tasks}) => ({tasks: [...tasks, {id: nanoid(), name, time}]}));
--- src/pages/generate-list.js
+++ src/pages/generate-list.js
@@ -1,14 +1,146 @@
 import {useRouter} from 'next/router';
 
 import StyledForm from '../components/Form/StyledForm';
+import useStore from '../hooks/useStore';
 
 export default function GenerateList() {
 	const router = useRouter();
+	const tasks = useStore(state => state.tasks);
+
+	function shuffle(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			let j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+	}
+
+	function selectTasks(
+		orderedTasks,
+		upperIndex,
+		number,
+		totalDuration,
+		avarageDuration,
+		lowerIndex = upperIndex - 1,
+		currentDuration = 0,
+		selectedTasks = [],
+		up = true
+	) {
+		if (
+			number === selectedTasks.length ||
+			currentDuration >= totalDuration ||
+			selectedTasks.length === orderedTasks.length
+		) {
+			return selectedTasks;
+		} else if (
+			up &&
+			upperIndex < orderedTasks.length &&
+			orderedTasks[upperIndex].time === avarageDuration
+		) {
+			selectedTasks.push(orderedTasks[upperIndex]);
+			currentDuration += orderedTasks[upperIndex].time;
+			upperIndex += 1;
+			return selectTasks(
+				orderedTasks,
+				upperIndex,
+				number,
+				totalDuration,
+				avarageDuration,
+				lowerIndex,
+				currentDuration,
+				selectedTasks,
+				up
+			);
+		} else if (up) {
+			if (upperIndex < orderedTasks.length) {
+				selectedTasks.push(orderedTasks[upperIndex]);
+				currentDuration += orderedTasks[upperIndex].time;
+				upperIndex += 1;
+				up = !up;
+				return selectTasks(
+					orderedTasks,
+					upperIndex,
+					number,
+					totalDuration,
+					avarageDuration,
+					lowerIndex,
+					currentDuration,
+					selectedTasks,
+					up
+				);
+			} else {
+				up = !up;
+				return selectTasks(
+					orderedTasks,
+					upperIndex,
+					number,
+					totalDuration,
+					avarageDuration,
+					lowerIndex,
+					currentDuration,
+					selectedTasks,
+					up
+				);
+			}
+		} else if (!up) {
+			if (lowerIndex >= 0) {
+				selectedTasks.push(orderedTasks[lowerIndex]);
+				currentDuration += orderedTasks[lowerIndex].time;
+				lowerIndex -= 1;
+				up = !up;
+				return selectTasks(
+					orderedTasks,
+					upperIndex,
+					number,
+					totalDuration,
+					avarageDuration,
+					lowerIndex,
+					currentDuration,
+					selectedTasks,
+					up
+				);
+			} else {
+				up = !up;
+				return selectTasks(
+					orderedTasks,
+					upperIndex,
+					number,
+					totalDuration,
+					avarageDuration,
+					lowerIndex,
+					currentDuration,
+					selectedTasks,
+					up
+				);
+			}
+		}
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		const formElements = event.target.elements;
+		const totalDuration = Number.parseInt(formElements.totalDuration.value, 10);
+		const number = Number.parseInt(formElements.numberOfTasks.value, 10);
+		const avarageDuration = totalDuration / number;
+		const shuffledTasks = [...tasks];
+		shuffle(shuffledTasks);
+		const orderedTasks = shuffledTasks.sort(
+			(firstTask, secondTask) => firstTask.time - secondTask.time
+		);
+		const index = orderedTasks.findIndex(task => task.time >= avarageDuration);
+		const selectedTasks = selectTasks(
+			orderedTasks,
+			index,
+			number,
+			totalDuration,
+			avarageDuration
+		);
+		console.log(selectedTasks);
+	}
 
 	return (
 		<>
 			<h1>Liste generieren</h1>
-			<StyledForm>
+			<StyledForm name="generateList" onSubmit={handleSubmit}>
 				<label htmlFor="totalDuration">Gesamtdauer in Minuten</label>
 				<input
 					type="number"
