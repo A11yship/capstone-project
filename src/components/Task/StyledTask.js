922cf846f34b2c4750ff68b878947a410600af18
Hannah Steil <h-steil@t-online.de>
2022-09-04 17:57:08 +0200

    Style dragable

--- src/components/Task/StyledTask.js
+++ src/components/Task/StyledTask.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 const StyledTask = styled.div`
-	border: 1px solid black;
+	margin: 7.5px 0;
+	padding: 3.5px 5px;
+	border-radius: 5px;
+	background-color: var(--brand-color);
+	color: var(--light-color);
 `;
 
 export default StyledTask;
