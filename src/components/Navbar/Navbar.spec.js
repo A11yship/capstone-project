9f1ec580dbc781496c9d4148660bd096244c791e
Hannah Steil <h-steil@t-online.de>
2022-09-04 15:32:30 +0200

    Fix tests

--- src/components/Navbar/Navbar.spec.js
+++ src/components/Navbar/Navbar.spec.js
@@ -7,6 +7,14 @@ import {render, screen} from '@testing-library/react';
 
 import Navbar from './Navbar';
 
+jest.mock('next/router', () => ({
+	useRouter() {
+		return {
+			push: jest.fn(),
+		};
+	},
+}));
+
 describe('Navbar component', () => {
 	it('should be displayed', () => {
 		render(<Navbar />);
