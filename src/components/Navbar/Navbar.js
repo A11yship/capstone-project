e8db390d3926bead112b8b2cbfb17d14f20de368
Hannah Steil <h-steil@t-online.de>
2022-09-04 15:27:12 +0200

    Update navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 import styled from 'styled-components';
 
 import Icon from '../Icon/Icon';
@@ -12,29 +13,28 @@ const StyledLink = styled.a`
 	width: 100%;
 	height: 100%;
 	border: 2px solid black;
-	border-radius: 2px;
+	border-radius: 1px;
 	background-color: var(--brand-color);
-	color: var(--light-color);
-	:active {
-		color: var(--dark-color);
-	}
+	color: ${({active}) => (active ? 'var(--dark-color);' : 'var(--light-color);')};
 `;
 
 export default function Navbar() {
+	const router = useRouter();
+
 	return (
 		<StyledNavbar>
 			<Link href="/task-list">
-				<StyledLink>
+				<StyledLink active={router.pathname === '/task-list'}>
 					<Icon variant="list" size="40px" />
 				</StyledLink>
 			</Link>
 			<Link href="/">
-				<StyledLink>
+				<StyledLink active={router.pathname === '/'}>
 					<Icon variant="play" size="40px" />
 				</StyledLink>
 			</Link>
 			<Link href="/create-task">
-				<StyledLink>
+				<StyledLink active={router.pathname === '/create-task'}>
 					<Icon variant="add" size="40px" />
 				</StyledLink>
 			</Link>
