2631f3cb441f68014b285cd8947f2947554a7abd
Hannah Steil <h-steil@t-online.de>
2022-09-05 11:54:35 +0200

    Clean add task page

--- src/pages/create-task.js
+++ src/pages/create-task.js
@@ -1,15 +1,11 @@
-import {useRouter} from 'next/router';
-
 import Form from '../components/Form/Form';
 import Layout from '../components/Layout';
 
 export default function CreateTask() {
-	const {query} = useRouter();
-
 	return (
 		<Layout>
-			<h1>Aufgabe {query.id ? 'bearbeiten' : 'hinzufügen'}</h1>
-			<Form task={query} />
+			<h1>Aufgabe hinzufügen</h1>
+			<Form />
 		</Layout>
 	);
 }
