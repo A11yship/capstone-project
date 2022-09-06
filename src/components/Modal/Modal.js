6425dabe617fc5679e99a22fe0bd2fb8f868ab94
Hannah Steil <h-steil@t-online.de>
2022-09-06 15:57:19 +0200

    Add wrapper container

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,16 +1,18 @@
 import Button from '../Button/Button';
 import StyledButtonContainer from '../ButtonContainer/StyledButtonContainer';
 
-import {StyledModal, StyledModalText} from './StyledModal';
+import {StyledModal, StyledModalText, StyledModalWrapper} from './StyledModal';
 
 export default function Modal({text, onCancel, onSubmit}) {
 	return (
-		<StyledModal>
-			<StyledModalText>{text}</StyledModalText>
-			<StyledButtonContainer>
-				<Button onClick={onCancel}>Nein</Button>
-				<Button onClick={onSubmit}>Ja</Button>
-			</StyledButtonContainer>
-		</StyledModal>
+		<StyledModalWrapper>
+			<StyledModal>
+				<StyledModalText>{text}</StyledModalText>
+				<StyledButtonContainer>
+					<Button onClick={onCancel}>Nein</Button>
+					<Button onClick={onSubmit}>Ja</Button>
+				</StyledButtonContainer>
+			</StyledModal>
+		</StyledModalWrapper>
 	);
 }
--- src/components/Modal/StyledModal.js
+++ src/components/Modal/StyledModal.js
@@ -1,18 +1,33 @@
 import styled from 'styled-components';
 
+const StyledModalWrapper = styled.div`
+	display: flex;
+	position: absolute;
+	z-index: 10;
+	top: 0;
+	left: 0;
+	flex-direction: column;
+	align-items: center;
+	width: 100vw;
+	height: 100vh;
+	background: rgb(35 32 32 / 72%);
+`;
+
 const StyledModal = styled.section`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	width: 80vw;
+	margin-top: 35vh;
 	padding: 10px;
 	gap: 5px;
 	border: 5px solid var(--dark-color);
 	border-radius: 20px;
+	background-color: var(--light-color);
 `;
 
 const StyledModalText = styled.p`
 	text-align: center;
 `;
 
-export {StyledModal, StyledModalText};
+export {StyledModalWrapper, StyledModal, StyledModalText};
