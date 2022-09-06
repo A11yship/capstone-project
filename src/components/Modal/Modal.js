3bd1f9d8c9091e196afb430841f61c94eb8cab0f
Hannah Steil <h-steil@t-online.de>
2022-09-06 13:08:19 +0200

    Style modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,12 @@
 import Button from '../Button/Button';
 import StyledButtonContainer from '../ButtonContainer/StyledButtonContainer';
 
-import StyledModal from './StyledModal';
+import {StyledModal, StyledModalText} from './StyledModal';
 
 export default function Modal({text, onCancel, onSubmit}) {
 	return (
 		<StyledModal>
-			<p>{text}</p>
+			<StyledModalText>{text}</StyledModalText>
 			<StyledButtonContainer>
 				<Button onClick={onCancel}>Nein</Button>
 				<Button onClick={onSubmit}>Ja</Button>
--- src/components/Modal/StyledModal.js
+++ src/components/Modal/StyledModal.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
 const StyledModal = styled.section`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 80vw;
 	padding: 10px;
 	gap: 5px;
 	border: 5px solid var(--dark-color);
 	border-radius: 20px;
 `;
 
-export default StyledModal;
+const StyledModalText = styled.p`
+	text-align: center;
+`;
+
+export {StyledModal, StyledModalText};
