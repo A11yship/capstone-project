73576ffe21de8618dc7485d415627d8ea7132345
Hannah Steil <h-steil@t-online.de>
2022-09-07 18:39:38 +0200

    Add name for the storage

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -3,30 +3,37 @@ import create from 'zustand';
 import {persist} from 'zustand/middleware';
 
 const useStore = create(
-	persist((set, get) => ({
-		tasks: [],
-		addTask(name, time) {
-			set(({tasks}) => ({tasks: [...tasks, {id: nanoid(), name, time}]}));
-		},
-		deleteTask(currentTaskId) {
-			set(({tasks}) => ({tasks: tasks.filter(task => task.id !== currentTaskId)}));
-			get().deleteFromCurrentTasks(currentTaskId);
-		},
-		editTask(currentId, name, time) {
-			set(({tasks}) => ({
-				tasks: tasks.map(task => (task.id === currentId ? {...task, name, time} : task)),
-			}));
-		},
-		currentTasks: [],
-		updateCurrentTasks(taskArray) {
-			set(() => ({currentTasks: [...taskArray]}));
-		},
-		deleteFromCurrentTasks(currentTaskId) {
-			set(({currentTasks}) => ({
-				currentTasks: currentTasks.filter(task => task.id !== currentTaskId),
-			}));
-		},
-	}))
+	persist(
+		(set, get) => ({
+			tasks: [],
+			addTask(name, time) {
+				set(({tasks}) => ({tasks: [...tasks, {id: nanoid(), name, time}]}));
+			},
+			deleteTask(currentTaskId) {
+				set(({tasks}) => ({tasks: tasks.filter(task => task.id !== currentTaskId)}));
+				get().deleteFromCurrentTasks(currentTaskId);
+			},
+			editTask(currentId, name, time) {
+				set(({tasks}) => ({
+					tasks: tasks.map(task =>
+						task.id === currentId ? {...task, name, time} : task
+					),
+				}));
+			},
+			currentTasks: [],
+			updateCurrentTasks(taskArray) {
+				set(() => ({currentTasks: [...taskArray]}));
+			},
+			deleteFromCurrentTasks(currentTaskId) {
+				set(({currentTasks}) => ({
+					currentTasks: currentTasks.filter(task => task.id !== currentTaskId),
+				}));
+			},
+		}),
+		{
+			name: 'TaskTimer',
+		}
+	)
 );
 
 export default useStore;
