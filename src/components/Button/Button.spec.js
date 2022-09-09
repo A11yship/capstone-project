aaa8c835bd61180de2b7be34f1971582d895067d
Hannah Steil <h-steil@t-online.de>
2022-09-09 11:24:12 +0200

    Add test

--- src/components/Button/Button.spec.js
+++ src/components/Button/Button.spec.js
@@ -6,6 +6,8 @@ import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+import Icon from '../Icon/Icon';
+
 import Button from './Button';
 
 describe('Button component', () => {
@@ -20,6 +22,17 @@ describe('Button component', () => {
 		const text = 'Click me';
 		render(<Button onClick={handleClick}>{text}</Button>);
 		const button = screen.getByRole('button', {name: text});
+		await userEvent.click(button);
+		expect(handleClick).toHaveBeenCalledTimes(1);
+	});
+	it('with icon should be clickable', async () => {
+		const handleClick = jest.fn();
+		render(
+			<Button variant="icon" onClick={handleClick}>
+				<Icon variant="done" />
+			</Button>
+		);
+		const button = screen.getByRole('button');
 		await userEvent.click(button);
 		expect(handleClick).toHaveBeenCalledTimes(1);
 	});
